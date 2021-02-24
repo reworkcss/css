a6959b17d993bd44aeef09a5a7a5ae6019b37514
fritz-c <4413963+fritz-c@users.noreply.github.com>
2021-02-24 08:53:09 -0600

    test: write tests for indent behavior, including failing empty string test

--- test/stringify.js
+++ test/stringify.js
@@ -110,3 +110,28 @@ describe('stringify(obj, {sourcemap: true})', function() {
     result.map.should.be.an.instanceOf(SourceMapGenerator);
   });
 });
+
+describe('stringify(obj, {indent: *})', function() {
+  var css =
+    '@media print {\n' +
+    '\tbody {\n' +
+    '\t\tbackground: #fff;\n' +
+    '\t}\n' +
+    '}';
+  var stylesheet = parse(css);
+
+  it('should default to two-space indent', function(){
+    var result = stringify(stylesheet);
+    result.should.eql(css.replace(/\t/g, '  '));
+  });
+
+  it('should indent according to the indent string', function(){
+    var result = stringify(stylesheet, { indent: '\t' });
+    result.should.eql(css);
+  });
+
+  it('should accept empty string for indent', function(){
+    var result = stringify(stylesheet, { indent: '' });
+    result.should.eql(css.replace(/\t/g, ''));
+  });
+});
