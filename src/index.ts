8be7da2abed6541417fb37dc285d356fdf5da461
holblin <holblin@holblin.com>
2023-03-14 16:33:27 -0700

    Release 4.3.0-beta.2 (#113)
    Co-authored-by: Jean-Philippe Zolesio <zolesio@adobe.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-export {default as parse} from './parse';
-export {default as stringify} from './stringify';
+import {default as parseFn} from './parse';
+import {default as stringifyFn} from './stringify';
+export const parse = parseFn;
+export const stringify = stringifyFn;
 export * from './type';
 export * from './CssParseError';
 export * from './CssPosition';
+export default {parse, stringify};
